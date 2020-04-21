0422c156c4b9b31be7af2fec39ab79d7bec7611e
randy.pierce <randy.pierce@noaa.gov>
2020-04-21 16:08:29 -0600

    refactor cbload into its own subdir so I could do complexity analysis

---
+++ METdbLoad/ush/cbload/cb_constants.py
@@ -0,0 +1,10 @@
+# CB constants - basically extend CN
+
+ID = "id"
+TYPE = "type"
+DATATYPE = "dataType"
+SUBSET = "subset"
+DATAFILE_ID = "dataFile_id"
+DATASOURCE_ID = "datasource_id"
+GEOLOCATION_ID = "geoLocation_id"
+DATA = "data"
